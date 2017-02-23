ae638b77db6bf021204480fbbd9d862405f6a62f
ACharLuk <acharluk@gmail.com>
2017-02-23 15:34:11 +0100

    Grid!

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
-function setup() {
-    createCanvas(800, 480);
+var scl = 10;
+var gridSize = 50;
+var grid;
 
+function setup() {
+    createCanvas(500, 500);
+
+    // Create grid
+    grid = [];
+    for (var i = 0; i < gridSize; i++) {
+        grid[i] = [];
+        for (var j = 0; j < gridSize; j++) {
+            grid[i][j] = random() < 0.5;
+        }
+    }
 }
 
 function draw() {
     background(0);
+
+    for (var i = 0; i < gridSize; i++) {
+        for (var j = 0; j < gridSize; j++) {
+            if (grid[i][j] === true) {
+                fill(255);
+            } else {
+                fill(0);
+            }
+            rect(i * scl, j * scl, scl, scl);
+        }
+    }
     
 }
